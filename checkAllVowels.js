36fb7c92a099e50e93db29180776aceaa90e3ec6
achimbaggins <achim_baggins@yahoo.com>
2017-07-25 20:19:43 +0700

    vowell oke

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,13 @@
+function checkVowell(str) {
+  let temp = []
+  for(let i = 0; i < str.length; i++){
+    if(str[i] === 'a' || str[i] === 'i' || str[i] === 'u' || str[i] === 'e' || str[i] === 'o'){
+      temp.push(str[i])
+    }
+  }
+  return temp.join('') === str ? true : false;
+}
 
+
+console.log(checkVowell('makan'));
+console.log(checkVowell('aiueo'));
