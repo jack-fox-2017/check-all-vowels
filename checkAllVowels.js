19209a97a1e3b4099455cc3ff82b5872275943a6
tezaharsony <teza.harsony230394@gmail.com>
2017-07-25 21:47:08 +0700

    Done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,21 @@
+function vowel(str) {
+var temp = []
 
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] === 'A' || str[i] === 'I' || str[i] === 'U' || str[i] === 'E' || str[i] === 'O' ) {
+      temp.push(str[i]);
+      // console.log('true');
+    }
+    // else {
+    //   // console.log('false');
+    // }
+  }
+  // console.log(temp);
+  if (temp.join('') === str) {
+    return true
+  }
+   return false
+}
+
+console.log(vowel('A'));
+console.log(vowel('AKU'));
