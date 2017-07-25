841720aec27551827e95678a4be311b14e15219f
gananggww <gananggww@gmail.com>
2017-07-25 21:33:52 +0700

    Check vowel done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,21 @@
-
+function isVowel(input) {
+  // let state = false;
+  let inputs = input.toLowerCase()
+  let arr = []
+  let arrSplit = inputs.split("")
+  console.log(arrSplit);
+  console.log(inputs);
+  for(let i = 0; i < arrSplit.length; i++){
+    if(arrSplit[i] === "a" || arrSplit[i] === "e" || arrSplit[i] === "i" || arrSplit[i] === "o" || arrSplit[i] === "u"){
+      arr.push(arrSplit[i])
+    }
+  }
+  // return arr
+  if(arr.join("") == inputs){
+    return true
+  }
+  return false
+}
+console.log(isVowel("AEI"));
+console.log(isVowel("aei"));
+console.log(isVowel("ganang"));
