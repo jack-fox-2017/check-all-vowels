48deb410c9f25ef8c0fe7eca85cead5c9d19b34e
alfandash <cumiasem91@gmail.com>
2017-07-25 16:48:26 +0700

    Release 0: Check All Vowels Done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,13 @@
+function checkAllVowels(arr) {
 
+  arr = arr.split('').map(function(x){return x.toLowerCase()}).filter(function(x){ if(x!=='a'&&x!=='i'&&x!=='u'&&x!=='e'&&x!=='o'){return x}}).join('');
+
+  return arr===''?true:false;
+}
+
+
+console.log(checkAllVowels('aaAaaaaaa')); // false
+console.log(checkAllVowels('aaaaabaaa')); // true
+console.log(checkAllVowels('AiuEo')); // true
+console.log(checkAllVowels('AiuEozwd')); //false
+console.log(checkAllVowels('Aid21313')); //false
