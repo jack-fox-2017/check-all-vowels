9683045b0690d4be7241d48e5436e8457ed5f30d
anggraito <anggraito.laras@gmail.com>
2017-07-25 17:15:59 +0700

    add function to check vowel

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,18 @@
+function vowel(input){
+  input = input.toLowerCase();
+  var save =[];
+  for (var i = 0; i < input.length; i++) {
+    if(input[i] === 'a' || input[i] === 'i' || input[i] === 'u' || input[i] === 'e' || input[i] === 'o' ){
+      save.push(input[i])
+    }
+  }
+  if(save.join('') == input){
+    return true
+  } else{
+    return false
+  }
+}
 
+console.log(vowel('AaO'));
+console.log(vowel('unknown'));
+console.log(vowel('ajax'));
