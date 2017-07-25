31e4df56a6604771ac8738aa5a2f7e8eb98f628d
anto <irianto223@gmail.com>
2017-07-25 15:55:28 +0700

    release-0 done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,21 @@
+'use strict'
 
+var isVowel = str => {
+  let arr = str.split('')
+  let count = 0
+  for (let i=0; i<arr.length; i++) {
+    if (arr[i]=='a' || arr[i]=='i' || arr[i]=='u' || arr[i]=='e' || arr[i]=='o') {
+      count++
+    }
+  }
+  if (count==arr.length) {
+    return true
+  }
+  else {
+    return false
+  }
+}
+
+console.log('aiuool', isVowel('aiuool'));
+console.log('aeeeaoe', isVowel('aeeeaoe'));
+console.log('kuda', isVowel('kuda'));
