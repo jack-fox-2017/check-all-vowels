a8d96ebe0986b02890da07b869ce9161d65c3e82
msrabbani <msrabbanii@gmail.com>
2017-07-25 21:55:09 +0700

    checkVowl - Done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,17 @@
+function checkAllVowl(str) {
+  let strSplit = str.split('')
+  // console.log(strSplit);
 
+  for(var x = 0; x < strSplit.length; x++){
+    if (strSplit[x] === "a" || strSplit[x] === "i" ||
+        strSplit[x] === "u" || strSplit[x] === "e" || strSplit[x] === "o" ) {
+      return true
+    } else {
+      return false
+    }
+  }
+}
+
+console.log(checkAllVowl("matahari"));
+console.log("=======================");
+console.log(checkAllVowl("ao"));
