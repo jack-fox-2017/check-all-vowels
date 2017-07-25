8aa6a2efa68ab3f42e9378bd11bc45c89f4e02c4
hariantara <hariantara.iputu@gmail.com>
2017-07-25 20:59:51 +0700

    checks all vowels done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,34 @@
+function vowels(input)
+{
+  let vocal = ['a','i','u','e','o']
+  let array = [];
+  let joins ;
+  let huruf = input.toString()
+  let string = huruf.toLowerCase()
+  let convert_input = string.split('');
+  //console.log(convert_input);
 
+  for(let i=0; i<convert_input.length; i++)
+  {
+
+    if(convert_input[i]==vocal[0] || convert_input[i] == vocal[1] || convert_input[i]==vocal[2] || convert_input[i]==vocal[3] || convert_input[i]==vocal[4])
+    {
+      array.push(convert_input[i]);
+      joins = array.join('');
+    }
+
+  }
+
+  //console.log(joins);
+
+  if(joins == string)
+  {
+    return true
+  }
+
+  return false
+
+}
+
+console.log(vowels("AIUEO"));
+console.log(vowels("Hari"));
