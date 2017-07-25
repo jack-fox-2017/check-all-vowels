9f647a7a2824fe4b364b8f4bb9a54102bdfdfbed
rizqjoe <rjoelisman@gmail.com>
2017-07-25 21:23:48 +0700

    checkVoc Done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,20 @@
+'use strict'
 
+
+function cekVoc(str) {
+var tampung = []
+  for (var i = 0; i < str.length; i++) {
+    if (str[i]=== 'a' || str[i]=== 'i' || str[i]=== 'u' || str[i]=== 'e' || str[i]=== 'o' ){
+      tampung.push(str[i])
+    }
+  }
+      if (tampung.join("") === str) {
+        return true;
+  }       else {
+      return false;
+  }
+}
+
+console.log(cekVoc('aku'));
+console.log(cekVoc('auio'));
+console.log(cekVoc('aiueo'));
