623c92205bb402ac85ec781af9db6eb859ac9906
Daniar Meiradi <daniarmeiradi@MacBook-Air.local>
2017-07-25 19:22:58 +0700

    check-all-vowels done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,20 @@
+function checkAllVowels(str){
 
+  let result = '';
+
+   for(let i=0;i<str.length;i++){
+     if(str[i]==='a' || str[i]==='e' || str[i]==='i' || str[i]==='o' || str[i]==='u'){
+       result = true;
+     }
+     else{
+       result =  false;
+       break;
+     }
+   }
+  return result;
+}
+
+console.log(checkAllVowels('oooo'));
+console.log(checkAllVowels('axiueo'));
+console.log(checkAllVowels('oueib'));
+console.log(checkAllVowels('eeeeee'));
