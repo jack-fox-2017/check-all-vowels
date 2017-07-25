0c484112017dbf87ec448d3e00249f72660b5aca
ahmadaidil <ahmdaidil@gmail.com>
2017-07-25 15:15:00 +0700

    check-all-vowels release 0

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,18 @@
+function checkAllVowels(str){
+  let vow = 'a i u e o'.split(' ');
+  let countVow = 0;
+  for(let i=0; i<vow.length; i++){
+      for(let j=0; j<str.length; j++){
+        if(str[j] == vow[i]){
+          countVow++;
+        }
+      }
+  }
+  if(countVow != str.length){
+    return false;
+  }
+  return true;
+}
 
+console.log(checkAllVowels('aaaaaaaa'));
+console.log(checkAllVowels('aaabiueo'));
