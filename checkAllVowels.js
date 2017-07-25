de4e745157842a91889e98b695d8e9e4c0a8ed51
Ahmad Nasikin <ahmad.nasikin09@gmail.com>
2017-07-25 20:15:23 +0700

    Tugas Vowels Selesai

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,19 @@
+'use stric'
 
+function checkVowels(huruf) {
+  var temp = [];
+  for(var i = 0; i < huruf.length; i++) {
+    if (huruf[i] === 'a' || huruf[i] === 'i' || huruf[i] === 'u' || huruf[i] === 'e' || huruf[i] === 'o') {
+      temp.push(huruf[i])
+    }
+  }
+  if (temp.join('') === huruf) {
+    return true
+  } else {
+    return false
+  }
+}
+
+
+console.log(checkVowels('adam'));
+console.log(checkVowels('aiueo'));
