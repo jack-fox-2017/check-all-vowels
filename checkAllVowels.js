55b54a57f43fe3d777c53507d95a3f1318bcbc73
Rahmat H <kickyou44@gmail.com>
2017-07-25 11:51:57 +0700

    check vowel selesai

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,20 @@
+function cekVokal(str) {
+	var obj = {
+		'a': true,
+		'i': true,
+		'u': true,
+		'e': true,
+		'o': true
+	}
 
+	for (var i = 0; i < str.length; i++) {
+		if (obj[str[i]] === undefined) return false
+	}
+	return true
+}
+
+console.log(cekVokal('aaa')); //true
+console.log(cekVokal('aaooo')); //true
+console.log(cekVokal('aiueo')); //true
+console.log(cekVokal('oioiuea')); //true
+console.log(cekVokal('aiuodoe')); //false
